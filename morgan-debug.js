c9a5e830429073549e7ee8e504d3d5ed42dc366c
ChiperSoft <chiper@chipersoft.com>
2014-08-07 13:31:54 -0700

    Initial commit

---
+++ morgan-debug.js
@@ -0,0 +1,19 @@
+var Debug = require('debug');
+var Morgan = require('morgan');
+var through2 = require('through2');
+
+module.exports = function (namespace, format, options) {
+	options = options || {};
+	var args = Array.prototype.slice(args, 1);
+	var debug = Debug(namespace);
+	var stream = through2(function (output, enc, callback) {
+		debug(output.toString('utf8').trim());
+		this.push(output);
+		callback();
+	});
+
+	// if options contains a stream, insert the debug stream in its place and pipe.
+	options.stream = options.stream ? stream.pipe(options.stream) : stream;
+
+	return Morgan(format, options);
+};
